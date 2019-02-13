6c06c2f0f8d9c971b6479bcabc3da219457d3f6a
Abhishek Jha <jhabhishek3797@gmail.com>
2019-02-14 01:39:53 +0530

    Second commit in LinkedList

---
+++ LinkedList/InbuiltLinkedList.java
@@ -0,0 +1,20 @@
+package ds;
+
+import java.util.*;
+
+public class InbuiltLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList <String> ll=new LinkedList<String>();
+		ll.add("This");
+		ll.add("Is");
+		ll.add("demo");
+		ll.add("Linked List");
+		
+		System.out.println(ll);
+
+	}
+
+}
